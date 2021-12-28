818d7a1dfd8d5bf76bc39e88868babb4035d74d6
Christos Gkatzos <cgkatzos@gmail.com>
2021-12-28 15:26:03 +0200

    Add initial code

---
+++ src/main/java/com/workshopbuilderui/workshopui/WorkshopUiApplication.java
@@ -0,0 +1,13 @@
+package com.workshopbuilderui.workshopui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workshopbuilderui/workshopui/WorkshopUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopbuilderui.workshopui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
